6a8529db1a793732800173689f18fe085fe07044
Dines <42099788+Hacko97@users.noreply.github.com>
2025-01-05 00:34:43 +0530

    two sum optimize

--- src/datastructure/hashtable/question1.java
+++ src/datastructure/hashtable/question1.java
@@ -2,30 +2,20 @@ package datastructure.hashtable;
 import java.util.*;
 class Solution {
     public static int[] twoSum(int[] nums, int target) {
-        Map<Integer,Integer> ht = new HashMap<>();
+        Map<Integer,Integer> complementNumbers = new HashMap<>();
 
         for(int i = 0; i < nums.length; i++){
-            ht.put(nums[i],i);
-        }
-
-        int[] result = new int[2];
-
-        for(int j = 0; j < nums.length; j++){
-            int curentValue = nums[j];
-            int searchValue = target - nums[j];
-            Object query = ht.get(searchValue);
-
-            if(query != null){
-                result[0] = j;
-                result[1] = (int) query;
+            int complement = target - nums[i];
+            if(complementNumbers.containsKey(complement)){
+                return new int[]{i,complementNumbers.get(nums[i])};
             }
-
+            complementNumbers.put(complement,i);
         }
 
-        return result;
+        return null;
     }
 
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(twoSum(new int[]{2, 0, 11, 14}, 9)));
+        System.out.println(Arrays.toString(twoSum(new int[]{2, 7, 11, 14}, 9)));
     }
 }
