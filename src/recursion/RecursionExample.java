74210842047b342800e80e31720baf26509551c2
Dines <42099788+mdineskumar@users.noreply.github.com>
2025-03-05 22:31:29 +0530

    recursion multiplication example

--- src/recursion/RecursionExample.java
+++ src/recursion/RecursionExample.java
@@ -1,5 +1,7 @@
 package recursion;
 
+import static java.lang.Math.abs;
+
 public class RecursionExample {
     public static int recursiveSum(int n){
         if (n == 0){
@@ -9,7 +11,36 @@ public class RecursionExample {
         return recursiveSum(n-1)+n;
     }
 
+    public static int multiplyMine(int a, int b){
+        if (abs(b) == 0){
+            return 0;
+        }
+        if (b < 0){
+            return -1*(multiplyMine(a,abs(b)-1) + a);
+        }
+        return multiplyMine(a,b-1) + a;
+
+    }
+
+    public static int multiply(int a, int b){
+        if (a == 0){
+            return 0;
+        }
+        return multiply(a-1,b)+b;
+    }
+
+    public static int multiplyFiset(int a, int b){
+        if (b == 0){
+            return 0;
+        }
+        if(b < 0){
+            return multiplyFiset(a,b+1) - a;
+        }
+        return multiplyFiset(a, b-1) + a;
+    }
+
     public static void main(String[] args) {
         System.out.println(recursiveSum(9));
+        System.out.println("multiplication: 4 * 5: " + multiplyMine(-4,5));
     }
 }
