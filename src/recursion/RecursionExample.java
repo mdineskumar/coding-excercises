e279922b89c30b737ace59d2060d3611f74359be
Dines <42099788+mdineskumar@users.noreply.github.com>
2025-03-09 00:04:19 +0530

    recursion loop through list example 2

--- src/recursion/RecursionExample.java
+++ src/recursion/RecursionExample.java
@@ -74,11 +74,14 @@ public class RecursionExample {
         return sumOddsFiset(0,lst);
     }
 
-    public static int productOfEvenNumbers(int[] lst){
+    public static int productOfEvenNumbersMine(int[] lst){
         return productOfEvens(0,lst);
     }
 
     public static int productOfEvens(int i,int[] lst){
+        if (lst.length == 0){
+            return 0;
+        }
         if (i == lst.length){
             return 1;
         }
@@ -89,13 +92,42 @@ public class RecursionExample {
         return productOfEvens(i+1,lst) * value;
     }
 
+    public static int productOfEvenNumbers(int[] lst){
+        Integer product = f(0,lst); // starting at 0
+        if (product == null){ // empty list or all the numbers in the list odd numbers
+            return 0;
+        }
+        return product;
+    }
+
+    public static Integer f(int i, int[] lst){
+        if (i == lst.length){ // base condition: current position at end of list
+            return null;//there is no product at the moment
+        }
+        Integer product = f(i+1,lst);
+
+        if (lst[i] % 2 != 0){ // check if current element is odd
+            return product; // current product or null : previous numbers also odd
+        }
+        if (product == null){//once we know current number is even then need to handle case where there is no product yet
+           return lst[i]; //return the element at current position
+        }
+        //integer overflow
+
+        return lst[i]*product; //otherwise multiply the current element and running product together
+    }
+
     public static void main(String[] args) {
         System.out.println(recursiveSum(9));
         System.out.println("multiplication: 4 * 5: " + multiplyMine(-4,5));
         int[] arr = new int[]{2,3,-4,5,11,6,7};
         int[] arr2 = new int[]{};
         System.out.println("sum of odd numbers: "+ sumOddsInList(arr));
-        System.out.println("product of even numbers: "+ productOfEvenNumbers(arr2));
+        System.out.println("product of even numbers: []: "+ productOfEvenNumbers(new int[]{}));
+        System.out.println("product of even numbers: [6,0]: "+ productOfEvenNumbers(new int[]{6,0}));
+        System.out.println("product of even numbers: [-8,-3,2]: "+ productOfEvenNumbers(new int[]{-8,-3,2}));
+        System.out.println("product of even numbers: [1,2,3,4,5]: "+ productOfEvenNumbers(new int[]{1,2,3,4,5}));
+        System.out.println("product of even numbers: [1,7,3]: "+ productOfEvenNumbers(new int[]{1,7,3}));
 
     }
 }
