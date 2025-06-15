0029c7ff1a947e18b8e27474e18e0093387277c4
Dineskumar <dines9748@gmail.com>
2025-06-15 11:47:46 +0530

    core java

---
+++ src/delete/ProductOfNumbers.java
@@ -0,0 +1,12 @@
+package delete;
+
+public class ProductOfNumbers {
+    public static void main(String[] args) {
+        int num1 = Integer.parseInt(args[0]);
+        int num2 = Integer.parseInt(args[1]);
+        int result = num1 * num2;
+
+        System.out.println("Product of two number is: "+result);
+
+    }
+}
