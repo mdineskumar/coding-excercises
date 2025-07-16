4ec077ed1eb2c39e3909352a8e96248393315c23
Dineskumar Murugesapillai <dineskumar.murugesapillai@zebra.com>
2025-07-16 12:34:37 +0530

    java 10 features

--- src/core_java_udemy/java10features/VarDemo.java
+++ src/core_java_udemy/java10features/VarDemo.java
@@ -1,15 +1,17 @@
 package core_java_udemy.java10features;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.function.Consumer;
 
 public class VarDemo {
+    //var str = "sinte";
+
     public static void main(String[] args) {
         var x = 20;
-        var y = "Strng";
+        var y ="dines";
+        y = "Dines";
+        //y = 145;
+
         var map = new HashMap<String, List<String>>();
         map.put("D", Arrays.asList("30","43"));
         for(var entry:map.entrySet()){
@@ -17,9 +19,13 @@ public class VarDemo {
         }
         //var s = null;
         Consumer<Integer> l = (i) ->{
-                var z = 10;
+                var z = 10;//can add var variables
                 System.out.println(i);
-        };
+        }; //cant change var
+
+        var list = new ArrayList<Integer>();
+        list.add(1);
+        //list.add("DUT");
 
     }
 }
