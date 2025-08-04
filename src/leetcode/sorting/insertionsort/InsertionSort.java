b48609ce2362dabbd27b3fbf8e4d6c5c4de84d08
Dineskumar <dines9748@gmail.com>
2025-08-04 22:32:23 +0530

    bubble sort implementation selection sort implementation insertion sort not implemented

---
+++ src/leetcode/sorting/insertionsort/InsertionSort.java
@@ -0,0 +1,15 @@
+package leetcode.sorting.insertionsort;
+
+import java.util.Arrays;
+
+public class InsertionSort {
+    public static void sort(int[] nums) {
+
+    }
+    public static void main(String[] args) {
+        int[] nums = {4,2,1,3};
+        sort(nums);
+
+        System.out.println(Arrays.toString(nums));
+    }
+}
