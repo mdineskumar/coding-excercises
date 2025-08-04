c68c17e3d75602b1c7a2ce875063accb7bdf8660
Dineskumar Murugesapillai <dineskumar.murugesapillai@zebra.com>
2025-08-04 17:09:00 +0530

    java questions

---
+++ src/javatest/Question14.java
@@ -0,0 +1,26 @@
+package javatest;
+
+class array_output1
+
+
+{
+
+    public static void main(String args[])
+
+    {
+
+        char array_variable [] = new char[10];
+
+        for (int i = 0; i < 10; ++i)
+
+        {
+
+            array_variable[i] = 'i';
+
+            System.out.print(array_variable[i] + "");
+
+        }
+
+    }
+
+}
---
+++ src/javatest/Question16.java
@@ -0,0 +1,31 @@
+package javatest;
+
+public class Question16 {
+    public static void main(String[] args) {
+        int a =10;
+        if(a<=0)
+
+
+        {
+
+            if(a==0)
+
+            {
+
+                System.out.println("1 ");
+
+            }
+
+            else
+
+            {
+
+                System.out.println("2 ");
+
+            }
+
+        }
+
+        System.out.println("3 ");
+    }
+}
