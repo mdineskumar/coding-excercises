ec5e531770c135a3fa892ecffe980e89712657f7
Dineskumar <dines9748@gmail.com>
2025-08-08 07:26:34 +0530

    InsertionSort

--- src/leetcode/sorting/insertionsort/InsertionSort.java
+++ src/leetcode/sorting/insertionsort/InsertionSort.java
@@ -4,7 +4,20 @@ import java.util.Arrays;
 
 public class InsertionSort {
     public static void sort(int[] nums) {
+        int n = nums.length;
+        //1. iterate through array
+        for (int i = 1; i < n; i++) {
+            //2.remove an element
+            int key = nums[i];
+            int j = i - 1;
+            //3. place it in correct spot - ?
+            while(j >= 0 &&nums[j] > key){
+                nums[j+1] = nums[j]; // nums[j+1] = nums[j]; it fails because i dont change j is iteration in sorted array.
+                j--;
+            }
+            nums[j+1] = key;
 
+        }
     }
     public static void main(String[] args) {
         int[] nums = {4,2,1,3};
