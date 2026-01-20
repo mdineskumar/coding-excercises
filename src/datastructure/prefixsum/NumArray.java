13abefd0e3787afb31466da576c579778b9c4a8a
Dineskumar Murugesapillai <dineskumar.murugesapillai@zebra.com>
2026-01-20 14:56:39 +0530

    PrefixSum

---
+++ src/datastructure/prefixsum/NumArray.java
@@ -0,0 +1,28 @@
+package datastructure.prefixsum;
+
+import java.util.Arrays;
+
+public class NumArray {
+    int[] prefixSum;
+    public NumArray(int[] nums) {
+        prefixSum = new int[nums.length + 1];
+
+        for (int i = 0; i < nums.length; i++) {
+            prefixSum[i+1] = prefixSum[i] + nums[i];
+        }
+        System.out.println("Prefix sum: "+ Arrays.toString(prefixSum));
+
+    }
+    public int sumRange(int left, int right) {
+        return prefixSum[right+1] - prefixSum[left];
+    }
+
+    public static void main(String[] args) {
+        NumArray numArray = new NumArray(new int[] {-2, 0, 3, -5, 2, -1});
+        System.out.println(numArray.sumRange(0,2));
+        System.out.println(numArray.sumRange(2,5));
+        System.out.println(numArray.sumRange(0,5));
+
+    }
+
+}
